5e0fbef6cd2e5ec564d70c6d633f65632db27625
Albir Tarsha <a.tarsha@yahoo.com>
2023-07-24 14:01:45 -0500

    Refactored check tests
    Standardized the test code for readability.

--- cypress/e2e/chess/check.cy.js
+++ cypress/e2e/chess/check.cy.js
@@ -1,5 +1,14 @@
-function hitRestartButton() {
-  cy.get("#restart-button").click()
+function validMovePlayer1(a, b, pName) {
+  validMoveA2B(a, b, 1, pName)
+}
+function validMovePlayer2(a, b, pName) {
+  validMoveA2B(a, b, 2, pName)
+}
+function invalidMovePlayer1(a, b, pName) {
+  invalidMoveA2B(a, b, 1, pName)
+}
+function invalidMovePlayer2(a, b, pName) {
+  invalidMoveA2B(a, b, 2, pName)
 }
 
 function validMoveA2B(a, b, player, pName) {
@@ -30,30 +39,14 @@ function invalidMoveA2B(a, b, player, pName) {
   cy.get(`div[tile_id="${a}"] div img[${imgSelector}]`).should("be.visible")
 }
 
-function castleA2B(a, b) {
-  let dataTransfer = new DataTransfer()
-  cy.get(`div[tile_id="${a}"] div img[alt="Your king"]`).should("be.visible")
-  cy.get(`div[tile_id="${a}"] div img[alt="Your king"]`).trigger("dragstart", {
-    dataTransfer,
-  })
-  cy.get(`div[tile_id="${b}"] div img`).trigger("drop", {
-    dataTransfer,
-  })
-  cy.get(`div[tile_id="${b}"] div img[alt="Your king"]`).should("be.visible")
-  if (b === 58)
-    cy.get(`div[tile_id="${59}"] div img[alt="Your rook"]`).should("be.visible")
-  else
-    cy.get(`div[tile_id="${61}"] div img[alt="Your rook"]`).should("be.visible")
-}
-
-function validPawnMoveA2B(a, b) {
-  let dataTransfer = new DataTransfer()
-  cy.get(`div[tile_id="${a}"] div img[alt="Your pawn"]`).trigger("dragstart", {
-    dataTransfer,
-  })
-  cy.get(`div[tile_id="${b}"] div img`).trigger("drop", {
-    dataTransfer,
-  })
+function checkPosition(a, player, pName) {
+  const playerStr = ["", "Your", "Computer's"]
+  let imgSelector = `alt="${playerStr[player]} ${pName}"`
+  if (a < 0 || a > 63) {
+    console.log(`Code Error: checkPosition(${a}, ${position}, ${pName})`)
+    return
+  }
+  cy.get(`div[tile_id="${a}"] div img[${imgSelector}]`).should("be.visible")
 }
 
 describe("Test check", () => {
@@ -62,53 +55,14 @@ describe("Test check", () => {
   })
 
   it("Cannot move king into check", () => {
-    let p = 52,
-      k = 60,
-      q = 59
-    // move kings near pawns
-    validPawnMoveA2B(p, (p -= 16))
-    validMoveA2B(k, (k -= 8), 1, "king")
-    validMoveA2B(k, (k -= 8), 1, "king")
-    validMoveA2B(k, (k += 1), 1, "king")
-    validMoveA2B(k, (k += 1), 1, "king")
-    validMoveA2B(k, (k -= 8), 1, "king")
-    validMoveA2B(k, (k -= 8), 1, "king")
-    // now try to illegally step into check by pawns
-    invalidMoveA2B(k, k - 8, 1, "king")
-    // use queen to kill pawn d7
-    validMoveA2B(q, (q -= 7), 1, "queen")
-    validMoveA2B(q, (q -= 9), 1, "queen")
-    validMoveA2B(q, (q -= 32), 1, "queen")
-    validMoveA2B(q, (q += 7), 1, "queen")
-    // now try to illegally step into check by bishop
-    invalidMoveA2B(k, k - 1, 1, "king")
-    // use queen to kill pawn e7
-    validMoveA2B(k, (k += 7), 1, "king")
-    validMoveA2B(q, (q += 2), 1, "queen")
-    validMoveA2B(q, (q -= 8), 1, "queen")
-    validMoveA2B(q, (q += 14), 1, "queen")
-    // now try to illegally step into check by queen
-    invalidMoveA2B(k, k - 7, 1, "king")
-    // move king to other side
-    validMoveA2B(k, (k += 7), 1, "king")
-    validPawnMoveA2B((p = 51), 1, (p -= 16))
-    validMoveA2B(q, (q -= 7), 1, "queen")
-    validMoveA2B(k, (k -= 1), 1, "king")
-    validMoveA2B(k, (k -= 9), 1, "king")
-    validMoveA2B(k, (k -= 9), 1, "king")
-    // use queen to kill pawn b7
-    validMoveA2B(q, (q += 7), 1, "queen")
-    validMoveA2B(q, (q -= 9), 1, "queen")
-    validMoveA2B(q, (q -= 8), 1, "queen")
-    // now try to illegally step into check by knight
-    invalidMoveA2B(k, k - 9, 1, "king")
-    invalidMoveA2B(k, k - 7, 1, "king")
-    // use queen to kill pawn a7
-    validMoveA2B(q, (q -= 1), 1, "queen")
-    validMoveA2B(q, (q += 1), 1, "queen")
-    // now try to illegally step into check by rook
-    invalidMoveA2B(k, k - 1, 1, "king")
-    // set up player 2 queenside castle
+    let pd1 = 51,
+      pe1 = 52,
+      qb1 = 58,
+      q1 = 59,
+      k1 = 60,
+      kb1 = 61,
+      kn1 = 62,
+      kr1 = 63
     let qr2 = 0,
       qn2 = 1,
       qb2 = 2,
@@ -116,16 +70,66 @@ describe("Test check", () => {
       k2 = 4,
       kb2 = 5,
       kn2 = 6,
-      kr2 = 7
-    validMoveA2B(qn2, (qn2 += 15), 2, "knight")
-    validMoveA2B(qb2, (qb2 += 45), 2, "bishop")
-    validMoveA2B(q2, (q2 += 16), 2, "queen")
+      kr2 = 7,
+      pe2 = 12,
+      pf2 = 13,
+      pg2 = 14,
+      ph2 = 15
+    // move kings near pawns
+    validMovePlayer1(pe1, (pe1 -= 16), "pawn")
+    validMovePlayer1(k1, (k1 -= 8), "king")
+    validMovePlayer1(k1, (k1 -= 8), "king")
+    validMovePlayer1(k1, (k1 += 1), "king")
+    validMovePlayer1(k1, (k1 += 1), "king")
+    validMovePlayer1(k1, (k1 -= 8), "king")
+    validMovePlayer1(k1, (k1 -= 8), "king")
+    // now try to illegally step into check by pawns
+    invalidMovePlayer1(k1, k1 - 8, "king")
+    // use queen to kill pawn d7
+    validMovePlayer1(q1, (q1 -= 7), "queen")
+    validMovePlayer1(q1, (q1 -= 9), "queen")
+    validMovePlayer1(q1, (q1 -= 32), "queen")
+    validMovePlayer1(q1, (q1 += 7), "queen")
+    // now try to illegally step into check by bishop
+    invalidMovePlayer1(k1, k1 - 1, "king")
+    // use queen to kill pawn e7
+    validMovePlayer1(k1, (k1 += 7), "king")
+    validMovePlayer1(q1, (q1 += 2), "queen")
+    validMovePlayer1(q1, (q1 -= 8), "queen")
+    validMovePlayer1(q1, (q1 += 14), "queen")
+    // now try to illegally step into check by queen
+    invalidMovePlayer1(k1, k1 - 7, "king")
+    // move king to other side
+    validMovePlayer1(k1, (k1 += 7), "king")
+    validMovePlayer1((pd1 = 51), (pd1 -= 16), "pawn")
+    validMovePlayer1(q1, (q1 -= 7), "queen")
+    validMovePlayer1(k1, (k1 -= 1), "king")
+    validMovePlayer1(k1, (k1 -= 9), "king")
+    validMovePlayer1(k1, (k1 -= 9), "king")
+    // use queen to kill pawn b7
+    validMovePlayer1(q1, (q1 += 7), "queen")
+    validMovePlayer1(q1, (q1 -= 9), "queen")
+    validMovePlayer1(q1, (q1 -= 8), "queen")
+    // now try to illegally step into check by knight
+    invalidMovePlayer1(k1, k1 - 9, "king")
+    invalidMovePlayer1(k1, k1 - 7, "king")
+    // use queen to kill pawn a7
+    validMovePlayer1(q1, (q1 -= 1), "queen")
+    validMovePlayer1(q1, (q1 += 1), "queen")
+    // now try to illegally step into check by rook
+    invalidMovePlayer1(k1, k1 - 1, "king")
+    // set up player 2 queenside castle
+    validMovePlayer2(qn2, (qn2 += 15), "knight")
+    validMovePlayer2(qb2, (qb2 += 45), "bishop")
+    validMovePlayer2(q2, (q2 += 16), "queen")
     // now try to illegally queenside castle covered by check by queen
-    invalidMoveA2B(k2, k2 - 2, 2, "king")
+    invalidMovePlayer2(k2, k2 - 2, "king")
+    checkPosition(qr2, 2, "rook")
     // back up player 1 queen
-    validMoveA2B(q, (q += 8), 1, "queen")
+    validMovePlayer1(q1, (q1 += 8), "queen")
     // now try to legally queenside castle while b8 is covered by check by queen
-    validMoveA2B(k2, (k2 -= 2), 2, "king")
+    validMovePlayer2(k2, (k2 -= 2), "king")
+    checkPosition((qr2 += 3), 2, "rook")
   })
 
   it("Cannot kingside castle into check", () => {
@@ -135,7 +139,8 @@ describe("Test check", () => {
       q1 = 59,
       k1 = 60,
       kb1 = 61,
-      kn1 = 62
+      kn1 = 62,
+      kr1 = 63
     let qr2 = 0,
       qn2 = 1,
       qb2 = 2,
@@ -150,28 +155,31 @@ describe("Test check", () => {
       ph2 = 15
 
     // set up player 1 for kingside castle
-    validMoveA2B(pe1, (pe1 -= 16), 1, "pawn")
-    validMoveA2B(kb1, (kb1 -= 36), 1, "bishop")
-    validMoveA2B(kn1, (kn1 -= 15), 1, "knight")
+    validMovePlayer1(pe1, (pe1 -= 16), "pawn")
+    validMovePlayer1(kb1, (kb1 -= 36), "bishop")
+    validMovePlayer1(kn1, (kn1 -= 15), "knight")
     // now try to legally kingside castle
-    validMoveA2B(k1, (k1 += 2), 1, "king")
+    validMovePlayer1(k1, (k1 += 2), "king")
+    checkPosition((kr1 -= 2), 1, "rook")
 
     // set up player 2 for kingside castle
-    validMoveA2B(kn2, (kn2 += 15), 2, "knight")
-    validMoveA2B(pe2, (pe2 += 8), 2, "pawn")
-    validMoveA2B(kb2, (kb2 += 14), 2, "bishop")
-    validMoveA2B(pg2, (pg2 += 8), 2, "pawn")
+    validMovePlayer2(kn2, (kn2 += 15), "knight")
+    validMovePlayer2(pe2, (pe2 += 8), "pawn")
+    validMovePlayer2(kb2, (kb2 += 14), "bishop")
+    validMovePlayer2(pg2, (pg2 += 8), "pawn")
 
     // set up player 2 to cover kingside castle at f8
-    validMoveA2B(pd1, (pd1 -= 8), 1, "pawn")
-    validMoveA2B(qb1, (qb1 -= 35), 1, "bishop")
+    validMovePlayer1(pd1, (pd1 -= 8), "pawn")
+    validMovePlayer1(qb1, (qb1 -= 35), "bishop")
 
     // now try to illegally queenside castle covered by check by bishop
-    invalidMoveA2B(k2, k2 + 2, 2, "king")
+    invalidMovePlayer2(k2, k2 + 2, "king")
+    checkPosition(qr2, 2, "rook")
     // move player 1 bishop to cover rook
-    validMoveA2B(qb1, (qb1 -= 9), 1, "bishop")
-    validMoveA2B(qb1, (qb1 += 7), 1, "bishop")
+    validMovePlayer1(qb1, (qb1 -= 9), "bishop")
+    validMovePlayer1(qb1, (qb1 += 7), "bishop")
     // now try to legally kingside castle while h8 is covered by bishop
-    validMoveA2B(k2, (k2 += 2), 2, "king")
+    validMovePlayer2(k2, (k2 += 2), "king")
+    checkPosition((kr2 -= 2), 2, "rook")
   })
 })
